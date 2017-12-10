e5514cfd9213eab0a255c6c3b7cae09c580ea00c
Soojeong <soojeongshin2@gmail.com>
2017-12-11 00:21:56 +0900

    Create a NewsLoader

---
+++ app/src/main/java/com/example/android/newsfeed/NewsLoader.java
@@ -0,0 +1,50 @@
+package com.example.android.newsfeed;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.example.android.newsfeed.utils.QueryUtils;
+
+import java.util.List;
+
+/**
+ * Created by sj on 12/10/2017.
+ */
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = NewsLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link NewsLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public NewsLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<News> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of news.
+        List<News> newsData = QueryUtils.fetchNewsData(mUrl);
+        return newsData;
+    }
+}
