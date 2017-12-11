f61a783208f80addaf3e0ee136ea4172bd6f659a
Soojeong <soojeongshin2@gmail.com>
2017-12-11 21:31:48 +0900

    Add comments to the EmptyRecyclerView

--- app/src/main/java/com/example/android/newsfeed/EmptyRecyclerView.java
+++ app/src/main/java/com/example/android/newsfeed/EmptyRecyclerView.java
@@ -6,13 +6,19 @@ import android.util.AttributeSet;
 import android.view.View;
 
 /**
- * Created by sj on 12/11/2017.
+ * EmptyRecyclerView is RecyclerView subclass that provides empty view support for RecyclerView
+ * to show or hide an empty view based on whether the adapter provided to the RecyclerView has
+ * data or not.
  */
 
 public class EmptyRecyclerView extends RecyclerView {
 
     private View mEmptyView;
 
+    /**
+     * The AdapterDataObserver calls checkIfEmpty() method every time, and it observes
+     * an event that changes the content of the adapter
+     */
     final private AdapterDataObserver observer = new AdapterDataObserver() {
         @Override
         public void onChanged() {
@@ -43,7 +49,14 @@ public class EmptyRecyclerView extends RecyclerView {
         super(context, attrs, defStyle);
     }
 
+    /**
+     * Checks if both mEmptyView and adapter are not null.
+     * Hide or show mEmptyView depending on the size of the data(item count) in the adapter.
+     */
     void checkIfEmpty() {
+        // If the item count provided by the adapter is equal to zero, make the empty View visible
+        // and hide the EmptyRecyclerView.
+        // Otherwise, hide the empty View and make the EmptyRecyclerView visible.
         if (mEmptyView != null && getAdapter() != null) {
             final boolean emptyViewVisible =
                     getAdapter().getItemCount() == 0;
@@ -66,6 +79,10 @@ public class EmptyRecyclerView extends RecyclerView {
         checkIfEmpty();
     }
 
+    /**
+     * Set an empty view on the EmptyRecyclerView
+     * @param emptyView refers to the empty state of the view
+     */
     public void setEmptyView(View emptyView) {
         mEmptyView = emptyView;
         checkIfEmpty();
