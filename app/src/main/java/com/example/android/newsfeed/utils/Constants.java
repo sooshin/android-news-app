455d437a8fd613f85a1de7a5068d033e9442d944
Soojeong <soojeongshin2@gmail.com>
2017-12-19 16:52:37 +0900

    Create Constants.java to extract strings into Constants

---
+++ app/src/main/java/com/example/android/newsfeed/utils/Constants.java
@@ -0,0 +1,27 @@
+package com.example.android.newsfeed.utils;
+
+/**
+ * Created by sj on 12/19/2017.
+ */
+
+public class Constants {
+
+    /**
+     * Create a private constructor because no one should ever create a {@link Constants} object.
+     */
+    private Constants() {
+    }
+
+    /**  Extract the key associated with the JSONObject */
+    public static final String JSON_KEY_RESPONSE = "response";
+    public static final String JSON_KEY_RESULTS = "results";
+    public static final String JSON_KEY_WEB_TITLE = "webTitle";
+    public static final String JSON_KEY_SECTION_NAME = "sectionName";
+    public static final String JSON_KEY_WEB_PUBLICATION_DATE = "webPublicationDate";
+    public static final String JSON_KEY_WEB_URL = "webUrl";
+    public static final String JSON_KEY_TAGS = "tags";
+    public static final String JSON_KEY_FIELDS = "fields";
+    public static final String JSON_KEY_THUMBNAIL = "thumbnail";
+    public static final String JSON_KEY_TRAIL_TEXT = "trailText";
+
+}
