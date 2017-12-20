fde77fc888a7a9be21c37e1eabf1880380787f40
Soojeong <soojeongshin2@gmail.com>
2017-12-20 23:28:34 +0900

    Implement Preference Summaries

--- app/src/main/java/com/example/android/newsfeed/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsfeed/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.newsfeed;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
@@ -20,12 +23,53 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
-    public static class NewsPreferenceFragment extends PreferenceFragment {
+    /**
+     * The NewsPreferenceFragment implements the Preference.OnPreferenceChangeListener interface
+     * to set up to listen for any Preference changes made by the user.
+     */
+    public static class NewsPreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            // Find the preference for number of items
+            Preference numOfItems = findPreference(getString(R.string.settings_number_of_items_key));
+            // bind the current preference value to be displayed
+            bindPreferenceSummaryToValue(numOfItems);
+        }
+
+        /**
+         * This method is called when the user has changed a Preference
+         *
+         * @param preference the changed Preference
+         * @param value the new value of the Preference
+         * @return True to update the state of the Preference with the new value
+         */
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            // Update the displayed preference summary (the UI) after it has been changed
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        /**
+         * Bind the value that is in SharedPreferences to what will show up in the preference summary
+         */
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            // Set the current NewsPreferenceFragment instance to listen for changes to the preference
+            // we pass in using
+            preference.setOnPreferenceChangeListener(this);
+
+            // Read the current value of the preference stored in the SharedPreferences on the device,
+            // and display that in the preference summary
+            SharedPreferences preferences =
+                    PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 
