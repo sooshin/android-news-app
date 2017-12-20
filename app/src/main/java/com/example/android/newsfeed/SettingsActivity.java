18dda00f1e2a0285413f0a23db5a51eaa326797f
Soojeong <soojeongshin2@gmail.com>
2017-12-20 18:16:14 +0900

    Handle up button in the SettingsActivity

--- app/src/main/java/com/example/android/newsfeed/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsfeed/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.android.newsfeed;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * The SettingsActivity is the activity that appears when a settings icon is clicked on.
@@ -13,9 +14,24 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        // Navigate with the app icon in the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
     public static class NewsPreferenceFragment extends PreferenceFragment {
 
+    }
+
+    // Go back to the MainActivity when up button in action bar is clicked on.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
